cd2b88dff87368a3c90174f7f1894716d261c22b
Michael Dorst <m@mdorst.net>
2023-09-14 02:38:03 -0700

    Add nodemailer

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import nodemailer from "nodemailer";
+
 const server = Bun.serve({
   async fetch(req) {
     const url = new URL(req.url);
@@ -6,9 +8,10 @@ const server = Bun.serve({
     }
     if (url.pathname === "/form-submit") {
       const formData = await req.formData();
-      const fname = formData.get("fname");
-      const lname = formData.get("lname");
-      console.log(fname, lname);
+      const name = formData.get("name");
+      const email = formData.get("email");
+      const comments = formData.get("comments");
+      await sendEmail(name, email, comments);
       return new Response("Form submitted!");
     }
     return new Response("Page Not Found", { status: 404 });
@@ -17,3 +20,24 @@ const server = Bun.serve({
 });
 
 console.log(`Listening on localhost:${server.port}`);
+
+async function sendEmail(name, email, comments) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: Bun.env.MAIL_USER,
+      pass: Bun.env.MAIL_PASS,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Form Submission" <m@mdorst.net>',
+    to: "info@briannamarie.design",
+    subject: `${name} would like to get in touch.`,
+    text: `Name: ${name}\nEmail: ${email}\nComments: ${comments}`,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
