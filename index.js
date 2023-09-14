3eaedc670ea6214c0d7d2fefaa2457784b6249ed
Michael Dorst <m@mdorst.net>
2023-09-13 18:44:53 -0700

    Set up basic form submission

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  async fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") {
+      return new Response(Bun.file("index.html"));
+    }
+    if (url.pathname === "/form-submit") {
+      const formData = await req.formData();
+      const fname = formData.get("fname");
+      const lname = formData.get("lname");
+      console.log(fname, lname);
+      return new Response("Form submitted!");
+    }
+    return new Response("Page Not Found", { status: 404 });
+  },
+  port: 3000,
+});
+
+console.log(`Listening on localhost:${server.port}`);
